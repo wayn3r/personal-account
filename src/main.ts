c0de385ef79aefa36d44b90894694d0795db5133
wayn3r <w9delacruz@gmail.com>
2022-05-30 23:29:06 -0400

    feat(cors): enable headers

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app/app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.enableCors({ origin: '*', allowedHeaders: '*' })
     app.setGlobalPrefix(`api/${process.env.APP_VERSION}`)
     app.useGlobalPipes(new ValidationPipe())
-    app.enableCors()
     await app.listen(process.env.APP_PORT || 8000)
 }
 bootstrap()

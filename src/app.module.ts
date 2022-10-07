719132f582bba06d30b05fccf65297fb620b1fe2
wayn3r <w9delacruz@gmail.com>
2022-10-07 09:14:26 -0400

    feat: add shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
-import { Module } from '@nestjs/common'
+import { Module, Global } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { TransactionModule } from '@/transaction/transaction.module'
 import { AuthModule } from '@/auth/auth.module'
+import { SharedModule } from '@/shared/shared.module'
 
+@Global()
 @Module({
   imports: [
     ConfigModule.forRoot({ expandVariables: true }),
     MongooseModule.forRoot(String(process.env.MONGO_URI)),
     TransactionModule,
     AuthModule,
+    SharedModule,
   ],
   controllers: [],
   providers: [],
+  exports: [SharedModule],
 })
 export class AppModule {}
